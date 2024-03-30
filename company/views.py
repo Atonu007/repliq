5a5de74c1b33263fa5d017d3dc533020edee4437
Atonu Shaha <atonushaha@Atonus-MacBook-Air.local>
2024-03-30 16:50:02 +0600

    user login and veiewing user details

--- company/views.py
+++ company/views.py
@@ -29,3 +29,40 @@ class UserRegistrationView(APIView):
             return Response({'token': token, 'msg': 'Registration Successful'}, status=status.HTTP_201_CREATED)
         return Response({'msg': 'Registration Unsuccessful'}, status=status.HTTP_400_BAD_REQUEST)
 
+class UserLoginView(APIView):
+    """
+    API endpoint for user login.
+    """
+    def post(self, request, format=None):
+        serializer = UserLoginSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            email = serializer.data.get('email')
+            password = serializer.data.get('password')
+            
+            user = authenticate(email=email, password=password)
+            
+            if user is not None:
+                token = get_tokens_for_user(user)
+                return Response({'token': token, 'msg': 'Login Success'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'errors': {'non_field_errors': ['Email or Password is not valid']}}, status=status.HTTP_404_NOT_FOUND)
+            
+        return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+
+class UserProfileView(APIView):
+    """
+    API endpoint for retrieving user profile.
+    """
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        # Serialize user profile
+        user_profile_serializer = UserProfileSerializer(request.user)
+
+        # Prepare response data
+        response_data = {
+            'user_profile': user_profile_serializer.data,
+        }
+        return Response(response_data, status=status.HTTP_200_OK)
+
+
