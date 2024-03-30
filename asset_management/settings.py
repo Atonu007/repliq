3de2839172a3b6a9fa6db1dc5ef19e7a389a3e60
Atonu Shaha <atonushaha@Atonus-MacBook-Air.local>
2024-03-30 16:06:26 +0600

    user model added and register it fro admin

--- asset_management/settings.py
+++ asset_management/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    
     'rest_framework',
+    'rest_framework_simplejwt',
+    'company',
 ]
 
 MIDDLEWARE = [
@@ -124,6 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'company.User'
 
 
 
---
+++ company/admin.py
@@ -0,0 +1,37 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+
+class UserModelAdmin(BaseUserAdmin):
+    # Displayed columns in the admin list view
+    list_display = ('id', 'email', 'name', 'phone', 'address', 'is_admin')
+    
+    # Filter options on the right side in the admin
+    list_filter = ('is_admin',)
+    
+    # Fieldsets for user details in the admin interface
+    fieldsets = (
+        ('User Credentials', {'fields': ('name', 'password')}),
+        ('Personal info', {'fields': ('email', 'phone', 'address')}),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+
+    # Additional fields when adding a new user in the admin interface
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'phone', 'address', 'password1', 'password2'),
+        }),
+    )
+    
+    # Search fields for searching users in the admin
+    search_fields = ('email',)
+    
+    # Ordering of users in the admin interface
+    ordering = ('name', 'id')
+    
+    # Fields displayed horizontally for the user in the admin interface
+    filter_horizontal = ()
+
+# Register the User model with the custom admin configuration
+admin.site.register(User, UserModelAdmin)
